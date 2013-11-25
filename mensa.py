4d24ec72dfd7506966875d74518abf8f9481032d
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2013-11-25 09:21:44 +0000

    Fixed encoding. :/

--- mensa.py
+++ mensa.py
@@ -129,8 +129,7 @@ def scrape_table(table, force_date = None):
         labelList = map(lambda s: s, labelList)
         
         mealName = meal.text_content()
-        
-        categoryName = category.text.decode("iso-8859-1").encode("utf-8")
+        categoryName = category.text
         
         if len(mealName) > 0:
             output += compFormat(u"  <category name={}>\n", quoteattr(category.text))
@@ -146,7 +145,7 @@ def scrape_table(table, force_date = None):
 
 def scrape_daily(url):
     content = str(getContents(url))
-    xml = soupparser.fromstring(content)
+    xml = soupparser.fromstring(content, fromEncoding="iso-8859-1")
     
     tables = xml.xpath("//table[contains(@class, 'bill_of_fare')]")
     if len(tables) > 0:
